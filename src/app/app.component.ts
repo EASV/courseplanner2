ebe516b0f10685801c5b8675af2827d450ddea60
lbilde <ljb@easv.dk>
2017-02-11 02:55:15 +0100

    First demo and now with types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Course} from './Course';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Welcome to the Course Planner';
+  courses : Course[];
+
+  constructor(){
+    this.courses = [
+      {
+        year: 2017,
+        title: 'Angular2',
+        education: 'Computer Science',
+        academy: 'EASV',
+        lecturer: 'Lars Bilde'
+      },
+      {
+        year: 2017,
+        title: 'ITO',
+        education: 'Computer Science',
+        academy: 'EASV',
+        lecturer: 'Lars Bilde'
+      }];
+  }
 }
+
+
---
+++ src/app/course.ts
@@ -0,0 +1,7 @@
+export class Course {
+  academy?: string;
+  year: number;
+  title: string;
+  education: string;
+  lecturer: string;
+}
