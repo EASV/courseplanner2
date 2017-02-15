aefeb03284b7a8811eb0f5ca25e65de256e0ab81
lbilde <ljb@easv.dk>
2017-02-15 22:33:45 +0100

    Adding information to home page again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,6 @@ import {Course} from './Course';
 })
 export class AppComponent {
   courses : Course[];
-  toolbarTitle = 'Courseplanner2';
-
-  folders = [
-    {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
-    },
-    {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
-    }
-  ];
-  notes = [
-    {
-      name: 'Vacation Itinerary',
-      updated: new Date('2/20/16'),
-    },
-    {
-      name: 'Kitchen Remodel',
-      updated: new Date('1/18/16'),
-    }
-  ];
 
   constructor(){
     this.courses = [
